9300d88752ed5863dd5c9a31d412f7320f32bee3
Giovanna Chiba <gigichiba@gmail.com>
2017-11-07 10:14:27 -0200

    Die & Respawn

--- Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -11,6 +11,8 @@ namespace UnityStandardAssets._2D
         public float lookAheadReturnSpeed = 0.5f;
         public float lookAheadMoveThreshold = 0.1f;
 
+        public float  yMin = -3.5f;
+
         private float m_OffsetZ;
         private Vector3 m_LastTargetPosition;
         private Vector3 m_CurrentVelocity;
@@ -53,9 +55,20 @@ namespace UnityStandardAssets._2D
             Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
 
+            if (newPos.y < yMin) {
+                newPos = new Vector3(newPos.x, yMin, newPos.z);
+            }
+
             transform.position = newPos;
 
             m_LastTargetPosition = target.position;
+            }
+            else {
+                GameObject obj = GameObject.FindGameObjectWithTag("Player");
+                if (obj != null) {
+                    target = obj.transform;
+                    updateVariables();
+                }
             }
         }
     }
